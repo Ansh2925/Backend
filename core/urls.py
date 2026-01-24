cf4a45dac6d58dc283346b0fde3f81c2337f767e
@ns <anshe2925@gmail.com>
2026-01-24 17:11:11 +0530

    created curr_date function

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('home/', views.home)
+    path('home/', views.home),
+    path('date/', views.curr_date),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+from datetime import datetime
 
 def home(request):
     return HttpResponse("<h1>Home</h1>")
 
+def curr_date(request):
+    current = datetime.today().ctime()
+    return HttpResponse(f"""<h2> {current} </h2>""")
 
 
-# Create your views here.
+
+
