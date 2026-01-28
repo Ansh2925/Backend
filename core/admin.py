f7f936368a98a81c33e5321b67ec8a62846461c8
@ns <anshe2925@gmail.com>
2026-01-28 17:36:16 +0530

    Worked with models, CRUD operations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Drinks, DrinksCategory, Menu, Customer, Vehicle
 
 # Register your models here.
+# Model registration using admin.site.register() is required to access the model from the admin user interface.
+admin.site.register(Drinks)
+admin.site.register(DrinksCategory)
+admin.site.register(Menu)
+admin.site.register(Customer)
+admin.site.register(Vehicle)
--- core/models.py
+++ core/models.py
@@ -9,3 +9,18 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.name + " " + self.cuisine
+
+class DrinksCategory(models.Model):
+    category_name = models.CharField(max_length=200)
+
+class Drinks(models.Model):
+    drink = models.CharField(max_length=200)
+    price = models.IntegerField()
+    category_id = models.ForeignKey(DrinksCategory, on_delete=models.PROTECT, default = None)
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='Vehicle')
